39db6437e57260a7a108b2f2c0505036690d41db
Oliver Fleckenstein <oliverfl@live.dk>
2020-11-12 16:21:18 +0800

    Added script to setup eslint with TypeScript and Prettier

---
+++ scripts/js-lint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+yarn add --dev eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-react prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-react-hooks
+cp $dir/.eslintrc.js .
+cp $dir/.prettierrc.json .
+
