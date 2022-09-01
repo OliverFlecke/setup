6eb789b3140828227abffab14898001f8f9b0ca5
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-01 16:46:35 +0100

    Add tools direction with script to install paq

---
+++ tools/install-nvim-paq.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+# Installs paq, a package manager for neovim
+
+git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
